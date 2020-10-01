c92effc263cedd9b6fba7d8c2a0cabb121d8d3c9
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2020-10-01 18:43:33 -0300

    Adding description in default migration

--- app/db/Migrations/Version20200919151041.php
+++ app/db/Migrations/Version20200919151041.php
@@ -14,7 +14,7 @@ final class Version20200919151041 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create users table';
     }
 
     public function up(Schema $schema): void
